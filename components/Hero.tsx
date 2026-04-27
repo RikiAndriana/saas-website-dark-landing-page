f719fdf7e9800836b778a19d6bba94c2940af3e8
RikiAndriana <rikiandrianakuliah@gmail.com>
2026-04-28 00:29:01 +0700

    fix(ui): prevent content overflow in responsive layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ import { motion } from "motion/react";
 
 export const Hero = () => {
   return (
-    <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-18 sm:py-24 relative overflow-x-clip">
+    <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-18 sm:py-24 relative overflow-clip">
       <div className="absolute h-[375px] w-[750px] sm:h-[768px] sm:w-[1536px] lg:w-[2400px] lg:h-[2400px] xl:w-[4400px] xl:h-[1000px] bg-black rounded-[100%] left-1/2 -translate-x-1/2 border border-[#B48CDE] bg-[radial-gradient(closest-side,#000_82%,#9560EB)] top-[calc(100%-96px)] sm:top-[calc(100%-120px)] xl:top-[calc(100%-170px)]"></div>
       <div className="container relative">
         <div className="flex items-center justify-center">
